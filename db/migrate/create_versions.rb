42bb9a1797a67e7ea7309d5bc3bac83e613df1f7
Mike Roach <mroach@mroach.com>
2017-07-19 23:30:40 -0400

    fix: object_changes for versions

--- db/migrate/create_versions.rb
+++ db/migrate/create_versions.rb
@@ -8,6 +8,7 @@ class CreateVersions < ActiveRecord::Migration[5.1]
       t.string   :event,     null: false
       t.string   :whodunnit
       t.text     :object
+      t.text     :object_changes
       t.datetime :created_at
     end
     add_index :versions, %i(item_type item_id)
